f334799364bd2a0be59dfa1d8cf5cd1afbdb223c
余阿伟 <awei.yu@2zhuzi.com>
2017-03-17 17:54:05 +0800

    增加代码提示相关的注释

--- date-internal.js
+++ date-internal.js
@@ -3,11 +3,19 @@
 /**
  * Created by aweiu on 17/3/17.
  */
-function DateInternal(unitSizes) {
+/**
+ * @param {Array} unitSizes - 单位对应的毫秒数组
+ */
+function DateInternal (unitSizes) {
   this.unitSizes = unitSizes.sort(function (unitSize1, unitSize2) {
     return unitSize1 < unitSize2;
   });
 }
+/**
+ * @param {Date} dateTime1 - 待比较的日期1
+ * @param {Date} [dateTime2 = now] - 待比较的日期2
+ * @returns {Array} - 按初始化单位的从大到小顺序计算出的间隔数组
+ */
 DateInternal.prototype.get = function (dateTime1) {
   var dateTime2 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : new Date();
 

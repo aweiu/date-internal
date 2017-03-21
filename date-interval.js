9dd3bec8daf4291b11dd5f1800f81dc4e968adfd
余阿伟 <awei.yu@2zhuzi.com>
2017-03-21 12:59:40 +0800

    更名=>date-interval

--- date-interval.js
+++ date-interval.js
@@ -6,7 +6,7 @@
 /**
  * @param {Array} unitSizes - 单位对应的毫秒数组
  */
-function DateInternal (unitSizes) {
+function DateInterval(unitSizes) {
   this.unitSizes = unitSizes.sort(function (unitSize1, unitSize2) {
     return unitSize1 < unitSize2;
   });
@@ -16,18 +16,18 @@ function DateInternal (unitSizes) {
  * @param {Date} [dateTime2 = now] - 待比较的日期2
  * @returns {Array} - 按初始化单位的从大到小顺序计算出的间隔数组
  */
-DateInternal.prototype.get = function (dateTime1) {
+DateInterval.prototype.get = function (dateTime1) {
   var dateTime2 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : new Date();
 
   var rs = [];
-  var internal = dateTime2 - dateTime1;
+  var interval = dateTime2 - dateTime1;
   for (var i = 0; i < this.unitSizes.length; i++) {
     var unitSize = this.unitSizes[i];
-    if (internal > unitSize) {
-      rs.push(parseInt(internal / unitSize));
-      internal = internal % unitSize;
+    if (interval > unitSize) {
+      rs.push(parseInt(interval / unitSize));
+      interval = interval % unitSize;
     } else rs.push(0);
   }
   return rs;
 };
-module.exports = DateInternal;
+module.exports = DateInterval;
--- dev/date-interval.js
+++ dev/date-interval.js
@@ -4,7 +4,7 @@
 /**
  * @param {Array} unitSizes - 单位对应的毫秒数组
  */
-function DateInternal (unitSizes) {
+function DateInterval (unitSizes) {
   this.unitSizes = unitSizes.sort((unitSize1, unitSize2) => unitSize1 < unitSize2)
 }
 /**
@@ -12,16 +12,16 @@ function DateInternal (unitSizes) {
  * @param {Date} [dateTime2 = now] - 待比较的日期2
  * @returns {Array} - 按初始化单位的从大到小顺序计算出的间隔数组
  */
-DateInternal.prototype.get = function (dateTime1, dateTime2 = new Date()) {
+DateInterval.prototype.get = function (dateTime1, dateTime2 = new Date()) {
   var rs = []
-  var internal = dateTime2 - dateTime1
+  var interval = dateTime2 - dateTime1
   for (var i = 0; i < this.unitSizes.length; i++) {
     var unitSize = this.unitSizes[i]
-    if (internal > unitSize) {
-      rs.push(parseInt(internal / unitSize))
-      internal = internal % unitSize
+    if (interval > unitSize) {
+      rs.push(parseInt(interval / unitSize))
+      interval = interval % unitSize
     } else rs.push(0)
   }
   return rs
 }
-export default DateInternal
+export default DateInterval
